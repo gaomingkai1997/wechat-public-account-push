c077937cf9498c2347f5c334f5a45e7b18dca083
gaomingkai1997 <40687329+gaomingkai1997@users.noreply.github.com>
2022-10-14 17:47:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,51 +13,56 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '上海',
+  CITY: '上海',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '亲爱的老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ov6G45i8KdAYgiyoRmLfyzqRjFHw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'Fv2wxqj5UKyLjHGAbcAx1YFfzVIrwHqhmQTeJE5JaAs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '01-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+        //{
+          //type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        //},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '自己', year: '1997', date: '01-03',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '生日', name: '最爱的老公', year: '1997', date: '03-08',
         },
+        {
+          type: '生日', name: '呆妈妈', year: '1974', date: '09-28',
+        }
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2020-07-11' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2021-05-01' },
+        
+        { keyword: 'certificate_day', date: '2021-03-22' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'CqxpY4opewG6bdtdZT5jzE-toitVYJT0E68WUWhW_iE',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ov6G45i8KdAYgiyoRmLfyzqRjFHw',
     }
   ],
 
